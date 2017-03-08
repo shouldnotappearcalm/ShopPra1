85f91a4e697b29cd3e20eac851322d449914a9ec
shouldnotappearcalm <1191465097@qq.com>
2017-03-08 14:49:16 +0800

    完成sitemesh3的配置，项目选择使用sitemesh装饰器框架

---
+++ Shop/src/main/java/com/gzr/controller/consumer/ConsumerAction.java
@@ -0,0 +1,13 @@
+package com.gzr.controller.consumer;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by GZR on 2017/3/8.
+ */
+@Controller(value = "/consumer")
+public class ConsumerAction {
+
+    
+
+}
